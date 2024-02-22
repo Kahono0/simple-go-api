126209dbc481ff08bd96c6285d354d84113b2d15
kahono0 <kahonojackson@gmail.com>
2024-02-22 17:58:58 +0300

    setup structure for auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,16 +13,52 @@ import (
 
 const defaultPort = "8080"
 
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	//set token as cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:  "token",
+		Value: "token",
+	})
+	w.WriteHeader(http.StatusOK)
+	//redirect to the graphiql playground
+	http.Redirect(w, r, "/graph", http.StatusSeeOther)
+}
+
+func authMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//check if the cookie is set
+		cookie, err := r.Cookie("token")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Unauthorized")
+		}
+		//check if the cookie value is correct
+		if cookie.Value != "token" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Unauthorized")
+		}
+		//if the cookie is set and the value is correct, call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	http.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok"}`))
+	}))
+
+	http.Handle("/auth", http.HandlerFunc(authenticate))
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/graph", authMiddleWare(playground.Handler("GraphQL playground", "/query")))
+	http.Handle("/query", authMiddleWare(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
